314cfaa49ce4a032d881ae4877b3a12b297f47bd
thebongy <rishit.bansal0@gmail.com>
2017-05-09 17:13:24 +0530

    Refact: Code restructured to use the same popup for office data and election data

--- resources/app/js/edit.ts
+++ resources/app/js/edit.ts
@@ -4,35 +4,86 @@ const { remote, ipcRenderer }: { remote: Electron.Remote, ipcRenderer: Electron.
 const { dialog }: { dialog: Electron.Dialog } = remote;
 import * as election from '../model/election';
 
-let electionData: election.ElectionDataInterface = null;
+let electionData: election.ElectionDataInterface = null; // The current electionData Object for this window.
+let currentModalData = null; // The data currently in the popup election Modal.
 
-let electionDataFields = document.getElementsByClassName('electionData');
+let electionDataFields = document.getElementsByClassName('electionData'); // All the input fields in the popup modal.
 
+// The headings on the top of the page.
 let editTitle = document.getElementById('election-title') as HTMLHeadingElement;
 let editSubTitle = document.getElementById('election-subtitle') as HTMLHeadingElement;
 
+//Controls inside the election popup modal
 let editElectionBtn = document.getElementById('editElectionBtn');
 let editElectionModal = document.getElementById('editElection');
+let electionModalTitle = document.getElementById('electionModalTitle')
 let saveEditElection = document.getElementById('saveEditElection');
 let closeEditElection = document.getElementById('closeEditElection');
 
-function toggleEditElection() {
+
+// View/Hide the election popup modal
+function toggleEditElectionModal() {
     editElectionModal.classList.toggle('is-active');
 }
 
+// Open the popup modal to edit the Election's top-level data.
+function openEditElectionInfo() {
+    electionModalTitle.innerHTML = "Edit Election: " + electionData.name;
+    setModalData(electionData);
+    currentModalData = electionData;
+    saveEditElection.children[1].innerHTML = 'Save Changes';
+    toggleEditElectionModal();
+}
+
+// Open the popup modal to edit a specific election's data.
+function openEditOfficeInfo(data, newOffice = false) {
+    if (newOffice) {
+        editElectionModal.innerHTML = 'Create New Office';
+        saveEditElection.children[1].innerHTML = "Create New Office";
+        currentModalData = {candidates:[]};
+        resetEditElectionModal();
+    } else {
+        editElectionModal.innerHTML = 'Edit Office: ' + data.name;
+        saveEditElection.children[1].innerHTML = "Save Changes";
+        setModalData(data);
+        currentModalData = data;
+    }
+    toggleEditElectionModal();
+}
+
+// Clear all the fields in the edit ELection Modal.
+function resetEditElectionModal() {
+    let inputData: HTMLInputElement;
+    for (let i = 0; i < electionDataFields.length; i++) {
+        inputData = <HTMLInputElement>electionDataFields[i];
+        inputData.value = '';
+    }
+    let electionImage = <HTMLImageElement>electionImagePreview.firstElementChild;
+
+    electionImagePreview.style.display = 'none';
+    electionImage.src = '';
+
+    electionBackColorBtn.style.backgroundColor = '';
+    electionTitleColorBtn.style.backgroundColor = '';
+}
+
+// Open the popup modal.
 editElectionBtn.addEventListener('click', function () {
-    toggleEditElection();
+    openEditElectionInfo();
 })
 
+// Close the popup modal.
 closeEditElection.addEventListener('click', function () {
-    toggleEditElection();
-    resetElectionData();
+    toggleEditElectionModal();
 })
 
+// Save all the data currently in the popup modal into the currentData variable, and then send it to the main
+// processs for saving locally.
 saveEditElection.addEventListener('click', function () {
     let inputData: HTMLInputElement;
     let collectData = <Object>{};
 
+    // Check if fields are not blank
     for (let i = 0; i < electionDataFields.length; i++) {
         inputData = <HTMLInputElement>electionDataFields[i];
         if (inputData.value.replace(/\s/g, "").length == 0) {
@@ -43,29 +94,37 @@ saveEditElection.addEventListener('click', function () {
     }
 
     // All field were valid, now store the data
+
     for (let key in collectData) {
         if (collectData.hasOwnProperty(key)) {
-            electionData[key] = collectData[key];
+            currentModalData[key] = collectData[key];
         }
     }
 
-    resetElectionData();
-
+    // Ask the main Process to save the Data, display success message
     let result = ipcRenderer.sendSync('saveElectionData', electionData);
     if (result == 'Saved') {
-        let loadAsk = dialog.showMessageBox({ type: 'info', message: 'Success', buttons: ['Ok'], detail: electionData.name+" has been saved succesfully."})
+        let loadAsk = dialog.showMessageBox({ type: 'info', message: 'Success', buttons: ['Ok'], detail: electionData.name + " has been saved succesfully." })
     }
 
-    toggleEditElection();
+    // This is to make sure the title is updated, if the election name and subtitle were changed.
+    editTitle.innerHTML = electionData.name;
+    editSubTitle.innerHTML = electionData.description;
+
+    // Close the popup modal.
+    toggleEditElectionModal();
 })
 
+// Recieve electionData for the page from the main Processs
 ipcRenderer.on('loadElectionData', (event, arg: election.ElectionDataInterface) => {
     loadElection(arg);
 })
 
+// Image controls in the popup.
 let imageBtn = <HTMLButtonElement>document.getElementById('imageBtn');
 let imageInput = <HTMLInputElement>document.getElementById('imageInput');
 
+// Warning, electionImagePreview is the <figure> elem, not the child <img> elem.
 let electionImagePreview = document.getElementById('electionImgPreview');
 
 // Open file explorer window to select image.
@@ -80,6 +139,7 @@ imageBtn.addEventListener('click', () => {
     previewImage(imagePath, electionImagePreview, imageInput, electionData.image);
 })
 
+// Decide whether to preview the image or hide in case no image is was selected.
 function previewImage(imagePath, imagePreview, imageInput, initalValue = '') {
     let previewImage = imagePreview.firstElementChild;
     if (typeof imagePath != 'undefined') {
@@ -95,29 +155,29 @@ function previewImage(imagePath, imagePreview, imageInput, initalValue = '') {
     }
 }
 
+// Controls in the popup to change the Back Colour, and font colour.
 let electionTitleColorBtn = document.getElementById('electionTitleColorBtn');
 let electionBackColorBtn = document.getElementById('electionBackColorBtn');
 
 function loadElection(data: election.ElectionDataInterface) {
     electionData = data;
-    resetElectionData();
+    editTitle.innerHTML = data.name;
+    editSubTitle.innerHTML = data.description;
 }
 
-function resetElectionData() {
 // Set the current data values in the edit election popup.
-
-    editTitle.innerHTML = electionData.name;
-    editSubTitle.innerHTML = electionData.description;
+function setModalData(data: election.ElectionDataInterface) {
 
     let inputData: HTMLInputElement;
     for (let i = 0; i < electionDataFields.length; i++) {
         inputData = <HTMLInputElement>electionDataFields[i];
-        inputData.value = electionData[inputData.name]
+        inputData.value = data[inputData.name]
     }
     let electionImage = <HTMLImageElement>electionImagePreview.firstElementChild;
 
-    electionImage.src = electionData.image;
+    electionImage.src = data.image;
+    electionImagePreview.style.display = 'block';
 
-    electionBackColorBtn.style.backgroundColor = electionData.backColor;
-    electionTitleColorBtn.style.backgroundColor = electionData.fontColor;
+    electionBackColorBtn.style.backgroundColor = data.backColor;
+    electionTitleColorBtn.style.backgroundColor = data.fontColor;
 }
